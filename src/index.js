1a5df9c3c80c198aeb35f0c11ed256a1446130d7
ShustKristina <shust1991@mail.ru>
2018-03-04 20:39:57 +0300

    ready brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var stackArray = [];
+
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    stackArray[i] = bracketsConfig[i].join("");
+  }
+  for (var j = 0; j < str.length;) {
+    var brackets = str.substring(j, j + 2);
+    if (stackArray.includes(brackets)) {
+      str = str.replace(brackets, "");
+      j = 0;
+    }
+    else {
+      j++;
+    }
+  }
+  return str.length == 0;
 }
